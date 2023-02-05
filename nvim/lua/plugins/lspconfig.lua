0f5e72cbacc1f535b40d2ae7cb26fb0d89e6b33b
ChristophP <deedop@hotmail.de>
2023-02-05 23:38:08 +0100

    try lsp config setup

---
+++ nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,17 @@
+-- every spec file under config.plugins will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  "neovim/nvim-lspconfig",
+  ---@class PluginLspOpts
+  opts = {
+    ---@type lspconfig.options
+    servers = {
+      -- elmls will be automatically installed with mason and loaded with lspconfig
+      elmls = {},
+    },
+  },
+}
