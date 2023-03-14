fa8fad0185697bdc5f0b604730fd5c0aae914c29
Dido (Christoph Poelt) <christoph.poelt@valiton.com>
2023-03-14 14:13:50 +0100

    add comment about colorizer plugin

--- nvim/lua/plugins/nvchad-colorizer.lua
+++ nvim/lua/plugins/nvchad-colorizer.lua
@@ -1,3 +1,5 @@
+-- Originally added to get colors for tailwind classes as suggested in this video:
+-- https://www.youtube.com/watch?v=_NiWhZeR-MY
 return {
   "NvChad/nvim-colorizer.lua",
   opts = {
