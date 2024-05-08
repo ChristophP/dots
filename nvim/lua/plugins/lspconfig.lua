4e36279fe46c1715a255d094ef783bc46fd21507
ChristophP <deedop@hotmail.de>
2024-05-08 17:03:05 +0200

    fix eslint/prettier setup

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -7,13 +7,19 @@
 return {
   "neovim/nvim-lspconfig",
   ---@class PluginLspOpts
+
   opts = {
-    ---@type lspconfig.options
-    servers = {},
-  },
-  setup = {
-    -- terraformls = function(_, opts)
-    --   opts.capabilities.documentFormattingProvider = false
-    -- end,
+    servers = { eslint = {} },
+    setup = {
+      eslint = function()
+        require("lazyvim.util").lsp.on_attach(function(client)
+          if client.name == "eslint" then
+            client.server_capabilities.documentFormattingProvider = true
+          elseif client.name == "tsserver" then
+            client.server_capabilities.documentFormattingProvider = false
+          end
+        end)
+      end,
+    },
   },
 }
